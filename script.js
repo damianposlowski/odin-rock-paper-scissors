f56d61dafa3fef1e06088cf1888f4cf36949ff4d
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-01-12 16:54:02 +0100

    Display winner when condition is reached

--- script.js
+++ script.js
@@ -3,62 +3,62 @@ function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
-            return "rock";
+            return 'rock';
             break;
         case 1:
-            return "paper";
+            return 'paper';
             break;
         default:
-            return "scissors";
+            return 'scissors';
     }
 }
 
 // Compare computer choice with player choice and return results
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock") {
+    if (playerSelection === 'rock') {
         switch (computerSelection) {
-            case "paper":
-                console.log("You Lose! Paper beats Rock.");
-                return "computer";
+            case 'paper':
+                console.log('You Lose! Paper beats Rock.');
+                return 'computer';
                 break;
-            case "scissors":
-                console.log("You Win! Rock beats Scissors.");
-                return "player";
+            case 'scissors':
+                console.log('You Win! Rock beats Scissors.');
+                return 'player';
                 break;
             default:
-                console.log("It's a tie!");
+                console.log('It`s a tie!');
                 return 0;
         }
-    } else if (playerSelection === "paper") {
+    } else if (playerSelection === 'paper') {
         switch (computerSelection) {
-            case "rock":
-                console.log("You Win! Paper beats Rock.");
-                return "player";
+            case 'rock':
+                console.log('You Win! Paper beats Rock.');
+                return 'player';
                 break;
-            case "scissors":
-                console.log("You Lose! Scissors beats Paper.");
-                return "computer";
+            case 'scissors':
+                console.log('You Lose! Scissors beats Paper.');
+                return 'computer';
                 break;
             default:
-                console.log("It's a tie!");
+                console.log('It`s a tie!');
                 return 0;
         }
-    } else if (playerSelection === "scissors") {
+    } else if (playerSelection === 'scissors') {
         switch (computerSelection) {
-            case "rock":
-                console.log("You Lose! Rock beats Scissors.");
-                return "computer";
+            case 'rock':
+                console.log('You Lose! Rock beats Scissors.');
+                return 'computer';
                 break;
-            case "paper":
-                console.log("You Win! Scissors beats Paper.");
-                return "player";
+            case 'paper':
+                console.log('You Win! Scissors beats Paper.');
+                return 'player';
                 break;
             default:
-                console.log("It's a tie!");
+                console.log('It`s a tie!');
                 return 0;
         }
     } else {
-        console.log("Wrong value selected.");
+        console.log('Wrong value selected.');
         return 0;
     }
 }
@@ -71,19 +71,19 @@ function playRound(playerSelection, computerSelection) {
 //     let playerSelection = getPlayerChoice();
 //     let computerSelection = getComputerChoice();
 //     let winner = playRound(playerSelection, computerSelection);
-//     if (winner === "player") {
+//     if (winner === 'player') {
 //         playerPoints++;
-//     } else if (winner === "computer") {
+//     } else if (winner === 'computer') {
 //         computerPoints++;
 //     }
 //     console.log(`Current score:\nPlayer: ${playerPoints} points\nComputer: ${computerPoints} points`);
 
     // if (playerPoints > computerPoints) {
-    //     console.log("Congratulations, you win!");
+    //     console.log('Congratulations, you win!');
     // } else if (playerPoints < computerPoints) {
-    //     console.log("You lose, better luck next time!");
+    //     console.log('You lose, better luck next time!');
     // } else {
-    //     console.log("No one wins, it's a tie!");
+    //     console.log('No one wins, it`s a tie!');
     // }
 // }
 
@@ -95,6 +95,7 @@ const buttons = document.querySelectorAll('button');
 
 const player = document.querySelector('#player-score');
 const computer = document.querySelector('#computer-score');
+const winnerMessage = document.querySelector('#winner');
 
 // Select buttons and add Event Listeners to playRound() with player's choice
 buttons.forEach(button => button.addEventListener('click',() => {
@@ -103,15 +104,18 @@ buttons.forEach(button => button.addEventListener('click',() => {
     let winner = playRound(playerSelection, computerSelection);
 
     // Display results in DOM after each round
-    if (winner === "player") {
+    if (winner === 'player') {
         playerPoints++;
-    } else if (winner === "computer") {
+    } else if (winner === 'computer') {
         computerPoints++;
     }
     player.textContent = 'Player: ' + playerPoints;
     computer.textContent = 'Computer: ' + computerPoints;
+
+    //  Display the winner when someone reaches 5 points
+    if(playerPoints === 5) {
+        winnerMessage.textContent = 'Congratulations, you win!';
+    } else if (computerPoints === 5) {
+        winnerMessage.textContent = 'You lose, better luck next time!';
+    }
 }));
-
-
-
-// Stop the game when someone reaches 5 points and display the winner
